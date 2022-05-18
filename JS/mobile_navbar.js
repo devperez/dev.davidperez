d57ce26d5d2cef3076e7509387bc15c566f974fe
devperez <advitameternam@gmail.com>
2022-05-18 14:29:47 +0200

    ajout des balises de méta données, modif du js de la barre de navigation

--- JS/mobile_navbar.js
+++ JS/mobile_navbar.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
-    //Menu button on click event
+    //Au clic sur le menu hamburger
     $('.mobile-nav-button').on('click', function() {
-      // Toggles a class on the menu button to transform the burger menu into a cross
+      // Toggle d'une class sur le bouton menu button pour transformer le menu hamburger en croix
         $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)" ).toggleClass( "mobile-nav-button__line--1");
         $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)" ).toggleClass( "mobile-nav-button__line--2");  
         $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)" ).toggleClass( "mobile-nav-button__line--3");  
 
-      // Toggles a class that slides the menu into view on the screen
+      // Toggle d'une classe qui fait glisser le menu à l'écran
         $('.mobile-menu').toggleClass('mobile-menu--open');
+        
         return false;
-    }); 
+    });
+    //Au clic sur l'un des élémenats du menu
+    $('.mobile-menu-item').on('click', function () {
+      //Toggle à nouveau de la classe pour faire réapparaître le menu hamburger
+      $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)" ).toggleClass( "mobile-nav-button__line--1");
+      $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)" ).toggleClass( "mobile-nav-button__line--2");  
+      $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)" ).toggleClass( "mobile-nav-button__line--3");  
+      
+      //Fermeture du menu mobile
+      $('.mobile-menu').toggleClass('mobile-menu--open');
+
+      return false;
+    });
 });
