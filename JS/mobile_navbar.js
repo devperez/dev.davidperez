807f51b51d2e3fd7f006ab75ba186334db4093ec
devperez <advitameternam@gmail.com>
2022-05-18 19:16:59 +0200

    modification du js de la navbar après avoir changé le menu hamburger par un menu de type bouton

--- JS/mobile_navbar.js
+++ JS/mobile_navbar.js
@@ -1,26 +1,29 @@
 $(document).ready(function () {
-    //Au clic sur le menu hamburger
-    $('.mobile-nav-button').on('click', function() {
-      // Toggle d'une class sur le bouton menu button pour transformer le menu hamburger en croix
-        $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)" ).toggleClass( "mobile-nav-button__line--1");
-        $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)" ).toggleClass( "mobile-nav-button__line--2");  
-        $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)" ).toggleClass( "mobile-nav-button__line--3");  
-
-      // Toggle d'une classe qui fait glisser le menu à l'écran
-        $('.mobile-menu').toggleClass('mobile-menu--open');
-        
-        return false;
-    });
-    //Au clic sur l'un des élémenats du menu
+  //Au clic sur le menu
+  $('.mobile-nav-button').on('click', function() {
+    // Toggle d'une classe qui fait glisser le menu à l'écran
+    $('.mobile-menu').toggleClass('mobile-menu--open');
+    
+    //changement du bouton
+    $('.mobile-nav-button').css('display', 'none');
+    $('.mobile-nav-fermer').css('display', 'block');
+  
+    //au clic sur l'un des éléments du menu, le menu se ferme et le bouton change à nouveau
     $('.mobile-menu-item').on('click', function () {
-      //Toggle à nouveau de la classe pour faire réapparaître le menu hamburger
-      $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)" ).toggleClass( "mobile-nav-button__line--1");
-      $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)" ).toggleClass( "mobile-nav-button__line--2");  
-      $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)" ).toggleClass( "mobile-nav-button__line--3");  
-      
-      //Fermeture du menu mobile
       $('.mobile-menu').toggleClass('mobile-menu--open');
-
-      return false;
+      $('.mobile-nav-button').css('display', 'block');
+      $('.mobile-nav-fermer').css('display', 'none');
     });
+  });
+
+
+  //Si on clique sur fermer directement
+  $('.mobile-nav-fermer').on('click', function () {
+    $('.mobile-menu').toggleClass('mobile-menu--open');
+    $('.mobile-nav-button').css('display', 'block');
+    $('.mobile-nav-fermer').css('display', 'none');
+  });
 });
+
+
+
