d9de22edb04897cf1b72d699cfb7ffee7b606ad9
devperez <advitameternam@gmail.com>
2022-05-20 12:41:38 +0200

    modifications sur les transitions entre pages, ajout de fakeloader, ajout de .htaccess

--- JS/transition.js
+++ JS/transition.js
@@ -2,7 +2,7 @@ window.transitionToPage = function(href) {
     document.querySelector('body').style.opacity = 0
     setTimeout(function() { 
     window.location.href = href
-}, 100)
+}, 0)
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
